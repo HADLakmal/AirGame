490cc25b8eba365367665dcbddef9371f446b26d
Damindu <damindu.14@cse.mrt.ac.lk>
2017-02-01 16:20:36 +0530

    begining

---
+++ GamePanel.java
@@ -0,0 +1,18 @@
+package com.example.damindu.airgame;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Damindu on 2/1/2017.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    public GamePanel(Context context){
+        super(context);
+
+        //add callback to the surfaceHolder to intercept event
+    }
+
+}
