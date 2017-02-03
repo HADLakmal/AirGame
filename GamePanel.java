09d8ee474693acc98c94ddf53a733a3604fe4afa
Damindu <damindu.14@cse.mrt.ac.lk>
2017-02-03 08:54:40 +0530

    Frame rate and canvas locked

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,9 @@
 package com.example.damindu.airgame;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -9,10 +12,57 @@ import android.view.SurfaceView;
  */
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 {
+    private MainThread thread;
     public GamePanel(Context context){
         super(context);
 
         //add callback to the surfaceHolder to intercept event
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(),this);
+
+        //make game panel focusable so it can handle event
+        setFocusable(true);
+
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        //we can safetly strat the game loop
+
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        //destroy
+        boolean retry = true;
+        while (retry){
+            try {
+                thread.setRunning(false);
+                thread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+                retry=false;
+            }
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
+
+    public void update(){
+
+    }
+
 
 }
